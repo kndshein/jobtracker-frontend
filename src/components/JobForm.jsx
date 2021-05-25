71f0d7a7d605c796a7f7fe62690c019e2227c150
Kaung Shein <nicholasling1412@gmail.com>
2021-05-25 00:24:07 -0400

    started Job Form

---
+++ src/components/JobForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const JobForm = (props) => {
+  const [formData, setFormData] = React.useState();
+
+  return (
+    <>
+      <div className="job-form-container">
+        <form>
+          <input></input>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default JobForm;
