480c2c877c40cb7e5720f8563acab7722865adef
Kaung Shein <nicholasling1412@gmail.com>
2021-05-25 22:17:58 -0400

    SCSS styling + Stars added

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import styles from "./Job.module.scss";
 
-const Job = ({ job }) => {
+import Stars from "../Stars/Stars";
+
+const Job = ({ job, backendUrl }) => {
   return (
-    <div className="job-container">
-      <div className="title">{job.job_title}</div>
-      <div className="company">{job.company_name}</div>
-      <div className="industry">{job.job_industry}</div>
-      <div className="description">{job.job_description}</div>
-      <div className="resume">{job.resume}</div>
-      <div className="coverletter">{job.coverletter}</div>
-      <div className="city">{job.location_city}</div>
-      <div className="state">{job.location_state}</div>
+    <div className={styles.job_container}>
+      <div className={styles.title}>{job.job_title}</div>
+      <div className={styles.logo_container}>
+        <img
+          className={styles.logo}
+          src="//logo.clearbit.com/spotify.com?size=80&greyscale=true"
+        />
+      </div>
+      <div className={styles.company}>{job.company_name}</div>
+      <div className={styles.status}>{job.status}</div>
+      <div className={styles.excitement}>{job.excitement}</div>
+      <Stars star={job.excitement} backendUrl={backendUrl} jobId={job.id} />
     </div>
   );
 };
