8d4fcbe95a598dd0bc666bbc728aae571e1c2e69
Kaung Shein <nicholasling1412@gmail.com>
2021-06-06 01:05:09 -0400

    Turn Accordion data into formData

--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -4,7 +4,7 @@ import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import { expandAccordion } from "../../apicalls/JobPage";
 
 import Stars from "../Stars/Stars";
-import JobExpanded from "./JobAccordion/JobAccordion";
+import JobExpanded from "../JobAccordion/JobAccordion";
 
 const Job = ({ job, setJobList }) => {
   const [expandedData, setExpandedData] = React.useState();
@@ -19,37 +19,26 @@ const Job = ({ job, setJobList }) => {
 
   return (
     <>
-      <div className={styles.job_container}>
+      <div
+        className={styles.job_container}
+        onClick={() => handleExpand(job.id)}
+      >
         <div className={styles.title}>{job.job_title}</div>
         <div className={styles.logo_container}>
           <img
             className={styles.logo}
             alt="spotify logo"
-            src="//logo.clearbit.com/spotify.com?size=80&greyscale=true"
+            src="https://logo.clearbit.com/spotify.com?size=80&greyscale=true"
           />
         </div>
         <div className={styles.company}>{job.company_name}</div>
         <div className={styles.status}>{job.status}</div>
         <div className={styles.excitement}>{job.excitement}</div>
         <div className={styles.excitement}>
-          <Stars star={job.excitement} jobId={job.id} />
+          <Stars star={job.excitement} jobId={job.id} clickable={false} />
         </div>
         <div className={styles.arrow}>
-          {expandedData ? (
-            <FaAngleUp
-              size={25}
-              onClick={() => {
-                handleExpand(job.id);
-              }}
-            />
-          ) : (
-            <FaAngleDown
-              size={25}
-              onClick={() => {
-                handleExpand(job.id);
-              }}
-            />
-          )}
+          {expandedData ? <FaAngleUp size={25} /> : <FaAngleDown size={25} />}
         </div>
       </div>
       {expandedData && (
---
+++ src/components/JobAccordion/JobAccordion.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { deleteJob } from "../../apicalls/JobPage";
+
+import Stars from "../Stars/Stars";
+
+const JobExpanded = ({ expandedData, handleExpand, setJobList }) => {
+  const emptyFormData = {
+    job_title: "",
+    company_name: "",
+    excitement: 0,
+    job_description: "",
+    resume: "",
+    coverletter: "",
+  };
+  const [formData, setFormData] = React.useState(emptyFormData);
+
+  React.useEffect(() => {
+    !expandedData ? setFormData(emptyFormData) : setFormData(expandedData);
+  }, [expandedData]);
+
+  const handleDelete = async () => {
+    await deleteJob(setJobList, formData.id);
+    handleExpand();
+  };
+
+  const handleOnChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="job-expanded-container">
+      <Stars star={formData.excitement} jobId={formData.id} clickable={true} />
+      <form>
+        <input
+          onChange={handleOnChange}
+          name="job_title"
+          value={formData.job_title}
+        />
+        <input
+          onChange={handleOnChange}
+          name="company_name"
+          value={formData.company_name}
+        />
+        <input
+          onChange={handleOnChange}
+          name="job_description"
+          value={formData.job_description}
+        />
+        <input
+          onChange={handleOnChange}
+          name="excitement"
+          value={formData.excitement}
+        />
+        <input
+          onChange={handleOnChange}
+          name="resume"
+          value={formData.resume}
+        />
+        <input
+          onChange={handleOnChange}
+          name="coverletter"
+          value={formData.coverletter}
+        />
+      </form>
+      <div>{formData.job_title}</div>
+      <div>{formData.company_name}</div>
+      <div>{formData.job_description}</div>
+      <div>{formData.excitement}</div>
+      <div>{formData.resume}</div>
+      <div>{formData.coverletter}</div>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
+
+export default JobExpanded;
